f31390231fef826fe6b3b78a5d4e78242b9a0be4
Bruno Zevallos <bruno_zevallos@usmp.pe>
2021-08-14 21:20:03 -0500

    Agregando Calculadora

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -5,9 +5,17 @@ namespace demomvc.Controllers
 {
     public class ContactoController:Controller
     {
+        
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "El contacto ya esta registrado";
+            return View("Index");
+        }
+
     }
 }
--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,10 +1,11 @@
+  
 namespace demomvc.Models
 {
     public class Contacto
     {
-        public string Correo { get; set }
-        public string Telefono { get; set }
-        public string Nombre { get; set }
-        public string Comentarios { get; set }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Nombre {get; set;}
+        public string Comentario {get; set; }
     }
 }
